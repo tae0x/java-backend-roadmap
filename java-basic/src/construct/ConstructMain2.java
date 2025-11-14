b5e11516e4d129c28420d56a7deeeb308840a7d7
taeho Jeong <t2222h@dgu.ac.kr>
2025-11-15 01:05:12 +0900

    study: java-basic - 섹션 5. 생성자

---
+++ java-basic/src/construct/ConstructMain2.java
@@ -0,0 +1,15 @@
+package construct;
+
+public class ConstructMain2 {
+
+    public static void main(String[] args) {
+        MemberConstruct number1 = new MemberConstruct("user1", 15, 90);
+        MemberConstruct number2 = new MemberConstruct("user2", 16);
+
+        MemberConstruct[] members = {number1, number2};
+
+        for (MemberConstruct s: members) {
+            System.out.println("이름:" + s.name + " 나이:" + s.age + " 성적:" + s.grade);
+        }
+    }
+}

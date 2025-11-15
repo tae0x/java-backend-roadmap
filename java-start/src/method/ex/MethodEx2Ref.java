bf279c230a2fd5bb44ee20f5e6a7f9c068979b30
taeho Jeong <t2222h@dgu.ac.kr>
2025-11-15 23:54:41 +0900

    review: java-start - 섹션 10. 메서드

---
+++ java-start/src/method/ex/MethodEx2Ref.java
@@ -0,0 +1,16 @@
+package method.ex;
+
+public class MethodEx2Ref {
+    public static void main(String[] args) {
+        String message = "Hello, world!";
+        printMessage("Hello, world", 3);
+        printMessage("Java", 5);
+        printMessage("hi", 7);
+    }
+
+    public static void printMessage(String message, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(message);
+        }
+    }
+}

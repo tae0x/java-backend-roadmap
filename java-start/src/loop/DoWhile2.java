03c1516c7fe9165248ef804f3a3ffaefb14b5e72
taeho Jeong <t2222h@dgu.ac.kr>
2025-11-15 21:28:27 +0900

    review: java-start - 섹션 6. 반복문

---
+++ java-start/src/loop/DoWhile2.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class DoWhile2 {
+
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            System.out.println("i = " + i);
+            i++;
+        } while (i < 3);
+    }
+}
